4a920aadc8105caf788d31376236d0fbfecf3f9e
ANGELA6XU <angela6.xu@gmail.com>
2018-08-08 14:11:58 +0800

    Add files via upload

---
+++ auto.py
@@ -0,0 +1,28 @@
+# from selenium import webdriver
+# import time
+
+# driver = webdriver.Chrome()
+# driver.get("https://www.baidu.com")
+# print(driver.title)
+# driver.find_element_by_id("kw").send_keys("selenium")
+# driver.find_element_by_id("su").click()
+# time.sleep(3)
+# driver.close()
+
+
+import pyautogui
+#print(pyautogui.size())
+
+#pyautogui.moveTo(800, 50)
+pyautogui.click(800, 50, duration = 1)
+pyautogui.click(800, 320, duration = 1)
+pyautogui.click(380, 105, duration = 1)
+pyautogui.click(380, 155, duration = 1)
+pyautogui.click(550, 500, duration = 1)
+pyautogui.click(550, 500)
+
+#pyautogui.moveTo(800, 800)
+#pyautogui.click(800,800, duration = 1)
+#pyautogui.moveTo(450,120)
+#pyautogui.click(450,120, duration = 1)
+
